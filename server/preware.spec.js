2ce610985e9321051368a75e66779275ccd13323
Amir irani <p30search@gmail.com>
2018-05-08 18:43:52 +0430

    complete preware tests

--- server/preware.spec.js
+++ server/preware.spec.js
@@ -13,6 +13,7 @@ describe('Preware', () => {
 
     let server;
     let adminCredentials;
+    let rootCredentails;
 
     before(async () => {
 
@@ -62,6 +63,42 @@ describe('Preware', () => {
 
         server.route({
             method: 'GET',
+            path: '/limited/to/foo/permission',
+            options: {
+                auth,
+                pre: [
+                    Preware.requireAdminHavePermission('foo')
+                ]
+            },
+            handler
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/limited/to/multiple/permissions',
+            options: {
+                auth,
+                pre: [
+                    Preware.requireAdminHavePermission(['foo', 'bar'])
+                ]
+            },
+            handler
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/root/or/limited/to/foo/permission',
+            options: {
+                auth,
+                pre: [
+                    Preware.requireRootOrHavePermission('foo')
+                ]
+            },
+            handler
+        });
+
+        server.route({
+            method: 'GET',
             path: '/just/not/the/root/user',
             options: {
                 auth,
@@ -72,9 +109,10 @@ describe('Preware', () => {
             handler
         });
 
-        adminCredentials = await Fixtures.Creds.createAdminUser(
-            'Ren Hoek', 'ren', 'baddog', 'ren@stimpy.show', ['Sales']
-        );
+        [rootCredentails, adminCredentials] = await Promise.all([
+            Fixtures.Creds.createRootAdminUser(),
+            Fixtures.Creds.createAdminUser('Ren Hoek', 'ren', 'baddog', 'ren@stimpy.show', ['Sales'], ['Bar'])
+        ]);
     });
 
     after(async () => {
@@ -107,9 +145,44 @@ describe('Preware', () => {
         expect(response.statusCode).to.equal(200);
     });
 
+    it('should prevent access when permission misses', async () => {
+
+        const request = {
+            method: 'GET',
+            url: '/limited/to/foo/permission',
+            credentials: adminCredentials
+        };
+        const response = await server.inject(request);
+
+        expect(response.statusCode).to.equal(403);
+    });
+
+    it('should grant access when permission hits', async () => {
+
+        const request = {
+            method: 'GET',
+            url: '/limited/to/multiple/permissions',
+            credentials: adminCredentials
+        };
+        const response = await server.inject(request);
+
+        expect(response.statusCode).to.equal(200);
+    });
+
+    it('should grant access when permission misses but root requested', async () => {
+
+        const request = {
+            method: 'GET',
+            url: '/root/or/limited/to/foo/permission',
+            credentials: rootCredentails
+        };
+        const response = await server.inject(request);
+
+        expect(response.statusCode).to.equal(200);
+    });
+
     it('should prevent access to the root user', async () => {
 
-        const rootCredentails = await Fixtures.Creds.createRootAdminUser();
         const request = {
             method: 'GET',
             url: '/just/not/the/root/user',
