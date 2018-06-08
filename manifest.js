56a508a3b710b762594ce840726016551210c028
Seyed AmirHosein Tavousi <p30search@gmail.com>
2018-06-08 14:54:14 +0430

    add cors and swagger options in manifest

--- manifest.js
+++ manifest.js
@@ -48,11 +48,68 @@ const devPlugins = [
     {
         plugin: 'hapi-swagger',
         options: {
+            securityDefinitions: {
+                'jwt': {
+                    'type': 'apiKey',
+                    'name': 'Authorization',
+                    'in': 'header'
+                }
+            },
+            security: [{ 'jwt': [] }],
             info: {
                 title: `${Config.get('/projectName')} API Documentation`,
-                version: Pack.version
+                version: Pack.version,
+                description: `Check out the **[Github Wiki](https://github.com/escommunity/fresh/wiki)** for common questions and how-tos.
+
+A few key things to be aware of:
+                The core User model found in the /endpoint/v1/users/ endpoints have these basic fields: _id, email, username, password, isActive, roles, timeCreated.
+                
+This framework decorates the core User models with additional role specific fields via mapping it to 1 or more roles. Fresh comes with 2 default roles, customers and admins.
+
+/endpoint/v1/accounts/ is the "customer account" role.
+                When users sign up via /endpoint/v1/signup the framework automatically creates a new User and a new Account (aka customer role) and links the two. Users can have multiple roles but each new instance of a role model can only be mapped to one user.
+                The customer Account role adds these additional fields for users who are customers: "name" (first, last), "notes", and "status". "Notes" allows admins to add notes to accounts.
+
+/endpoint/v1/admins/ is the "admin" role.
+                This role contains a "name" (first, last), "permissions", and "groups" property allowing you to assign multiple admin-groups. The first admin-group is "root" which is allowed to perform the "Root Scope" actions.
+
+More details on [Users, Roles & Groups](https://github.com/escommunity/fresh/wiki/Users,-Roles-&-Groups)
+                More details on [Admin & Admin Group Permissions](https://github.com/escommunity/fresh/wiki/Admin-&-Admin-Group-Permissions)`
             },
-            grouping: 'tags'
+            grouping: 'tags',
+            sortTags: 'name',
+            tags: [
+                {
+                    name: 'accounts',
+                    description: 'endpoints to interact with customer role.'
+                }, {
+                    name: 'admin-groups',
+                    description: 'endpoints to interact with admin groups.'
+                }, {
+                    name: 'admins',
+                    description: 'endpoints to interact with admin roles.'
+                }, {
+                    name: 'contact'
+                }, {
+                    name: 'login',
+                    description: 'endpoints for login flow.'
+                }, {
+                    name: 'logout'
+                }, {
+                    name: 'main'
+                }, {
+                    name: 'session',
+                    description: 'endpoints to interact with user sessions.'
+                }, {
+                    name: 'signup'
+                }, {
+                    name: 'statuses',
+                    description: 'endpoints to interact with customer role (account) statuses.'
+                }, {
+                    name: 'users',
+                    description: 'endpoints to interact with users (outside of roles)'
+                }
+            ]
         }
     },
     {
@@ -193,6 +250,12 @@ const manifest = {
                 }
             },
             files: { relativeTo: Config.get('/publicPath') },
+            cors: {
+                origin: ['*'],
+                maxAge: 86400,
+                headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match'],
+                credentials: true
+            },
             security: true
         },
         port: Config.get('/port/web')
